f56deea08912d7c13ade348e0e2e9cff65d27c22
tarek doha <tarikdoha80@gmail.com>
2021-11-03 20:36:10 +0100

    Implement Image class and the Frontend base folder

---
+++ Backend-Project/Migrations/20211103190143_Image.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend_Project.Migrations
+{
+    public partial class Image : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ImageId",
+                table: "Reports",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_ImageId",
+                table: "Reports",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Image_ImageId",
+                table: "Reports",
+                column: "ImageId",
+                principalTable: "Image",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Image_ImageId",
+                table: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_ImageId",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Reports");
+        }
+    }
+}
